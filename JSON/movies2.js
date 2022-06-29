378a1241e3a24fe99aca356ef30542909432aef3
Erik Visnievskij <erik903@mail.ru>
2022-06-29 09:22:53 +0300

    new

--- JSON/movies2.js
+++
@@ -1,8 +0,0 @@
-fetch('movies-list.json')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        // moviesList.map((movieId) => {
-        //     console.log(movieId);
-        // })
-    })
